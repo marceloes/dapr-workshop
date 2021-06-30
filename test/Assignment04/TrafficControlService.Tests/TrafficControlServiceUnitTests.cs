4b30952f8e0f4aa1f4e9c4317fb0fa19e9fb4164
Jordan Bean <jordanbean@microsoft.com>
2021-06-30 08:11:37 -0500

    initial Assignment04

---
+++ test/Assignment04/TrafficControlService.Tests/TrafficControlServiceUnitTests.cs
@@ -0,0 +1,54 @@
+using System;
+using Xunit;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Xunit.Sdk;
+using System.Text;
+
+namespace TrafficControlService.Tests
+{
+    public class TrafficControlServiceUnitTests
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        [Fact]
+        public async Task EntryCamTest()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+
+            HttpContent httpContent = new StringContent(@"{ ""lane"": 1, ""licenseNumber"": ""XT-346-Y"", ""timestamp"": ""2020-09-10T10:38:47"" }", 
+                                                        Encoding.UTF8, 
+                                                        "application/json");
+
+            HttpResponseMessage httpResponseMessage;
+            try {
+                httpResponseMessage = await client.PostAsync("http://localhost:3600/v1.0/invoke/TrafficControlService/method/entrycam", httpContent);
+            }
+            catch (Exception ex) {
+                throw new XunitException($"Unable to query endpoint. Error: ${ex.Message}");
+            }            
+
+            Assert.True(httpResponseMessage.IsSuccessStatusCode); 
+        }
+
+        [Fact]
+        public async Task ExitCamTest()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+
+            HttpContent httpContent = new StringContent(@"{ ""lane"": 1, ""licenseNumber"": ""XT-346-Y"", ""timestamp"": ""2020-09-10T10:38:52"" }", 
+                                                        Encoding.UTF8, 
+                                                        "application/json");
+
+            HttpResponseMessage httpResponseMessage;
+            try {
+                httpResponseMessage = await client.PostAsync("http://localhost:3600/v1.0/invoke/TrafficControlService/method/exitcam", httpContent);
+            }
+            catch (Exception ex) {
+                throw new XunitException($"Unable to query endpoint. Error: ${ex.Message}");
+            }            
+
+            Assert.True(httpResponseMessage.IsSuccessStatusCode); 
+        }
+    }
+}
